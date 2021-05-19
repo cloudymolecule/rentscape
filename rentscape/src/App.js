5806b9c181893d79a8996bb62708960c8c63bbb1
Cloudymolecule <baires82@gmail.com>
2021-05-19 14:59:40 -0400

    Add css

--- rentscape/src/App.js
+++ rentscape/src/App.js
@@ -2,8 +2,29 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1>Test</h1>
+    <div className='wrapper'>
+      <div className='container'>
+        <div className='nav'>
+          <div className='menu-left'>
+            menu-left
+          </div>
+          <div className='logo'>
+            logo
+          </div>
+          <div className='menu-right'>
+            menu-right
+          </div>
+        </div>
+        <div className='notifications'>
+          notifications
+        </div>
+        <div className='display'>
+          display
+        </div>
+        <div className='footer'>
+          footer
+        </div>
+      </div>
     </div>
   )
 }
