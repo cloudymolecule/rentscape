c327734c9922359911dafecd139822073659e4d4
Cloudymolecule <baires82@gmail.com>
2021-05-21 12:14:59 -0400

    Add flip animation to property cards

--- rentscape/src/components/Property.js
+++ rentscape/src/components/Property.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 const Property = (props) => {
     return (
-        <div className='property'>
-            <h3>{props.address}</h3>
-            <p>{props.photo}</p>
-            <p>{props.stars}</p>
+        <div className='flip-property'>
+            <div className='flip-property-inner'>
+                <div className='flip-property-front'>
+                    <h3>{props.address}</h3>
+                    <p>{props.photo}</p>
+                    <p>{props.stars}</p>
+                </div>
+                <div className="flip-property-back">
+                    <p>{props.photo}</p>
+                </div>
+            </div>
         </div>
     )
 }
