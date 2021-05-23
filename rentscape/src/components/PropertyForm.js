725b18a0678efbf0c4c222e82c4bb5da63b79b39
Cloudymolecule <baires82@gmail.com>
2021-05-23 13:14:36 -0400

    Update new property form

--- rentscape/src/components/PropertyForm.js
+++ rentscape/src/components/PropertyForm.js
@@ -11,22 +11,22 @@ export default class ReviewForm extends Component {
             <form className='new-property-form'>
                 <h1>Add a New Rental Property</h1>
                 
-                <div>
+                <div className='new-property-fields'>
                     <label>Address: </label>
                     <input />
                 </div>
                 
-                <div>
+                <div className='new-property-fields'>
                     <label>Address 2: </label>
                     <input />
                 </div>
 
-                <div>
+                <div className='new-property-fields'>
                     <label>Township: </label>
                     <input />
                 </div>
 
-                <div>
+                <div className='new-property-fields'>
                     <label>State: </label>
                     <select>
                             <option value='All'>All</option>
@@ -34,19 +34,24 @@ export default class ReviewForm extends Component {
                     </select>
                 </div>
 
-                <div>
-                    <label for="img">Select image: </label>
+                <div className='new-property-fields'>
+                    <label>Review Title: </label>
+                    <input />
+                </div>
+                
+                <div >
+                    <label>Review: </label>
+                    <br />
+                    <textarea rows="8" cols="50"/>
+                </div>
+
+                <div className='new-property-fields'>
+                    <label>Select Image: </label>
                     <input type="file" id="img" name="img" accept="image/*" />
                     {/* <input type="submit"></input> */}
                 </div>
 
-                <div>
-                    <label>Review: </label>
-                    <br />
-                    <textarea />
-                </div>
-
-                <div>
+                <div >
                     <button type='submit'>Add Property</button>
                 </div>
             </form>
