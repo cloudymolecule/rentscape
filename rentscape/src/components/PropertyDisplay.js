66bc08e074b7c10da128deafde35481228eee042
Cloudymolecule <baires82@gmail.com>
2021-05-27 11:05:19 -0400

    Update PropertyShow.js and css

--- rentscape/src/components/PropertyDisplay.js
+++ rentscape/src/components/PropertyDisplay.js
@@ -16,7 +16,21 @@ const PropertyDisplay = (props) => {
             </div>
             <div className='notifications'>NOTIFICATIONS</div>
             <div className='display'>
-                <PropertyShow />
+                <PropertyShow 
+                    address={property.address}
+                    address_2={property.address_2}
+                    cleanliness_rating={property.cleanliness_rating}
+                    id={property.id}
+                    image_url={property.image_url.url}
+                    landlord_rating={property.landlord_rating}
+                    neighbors_rating={property.neighbors_rating}
+                    overall_rating={property.overall_rating}
+                    price_rating={property.price_rating}
+                    review={property.review}
+                    review_title={property.review_title}
+                    state={property.state}
+                    township={property.township}
+                />
             </div>
             <div className='footer'>FOOTER</div>
         </>
--- rentscape/src/components/PropertyShow.js
+++ rentscape/src/components/PropertyShow.js
@@ -1,11 +1,34 @@
 import React from 'react'
 
-const PropertyShow = () => {
+const PropertyShow = (props) => {
     return (
-        <div>
-            <h1>HI</h1>
+        <div className="property-show">
+            <div className='property-show-info'>
+                <hr />
+                    <h2>{props.address}, {props.state}</h2>
+                <hr />
+                <ul>
+                    <li>{props.address} - {props.address_2}, {props.township} {props.state}</li>
+                    <li>Overall Rating: {props.overall_rating}</li>
+                    <li>Landlord Rating: {props.landlord_rating}</li>
+                    <li>Neighbors Rating: {props.neighbors_rating}</li>
+                    <li>Price Rating: {props.price_rating}</li>
+                    <li>Cleanliness Rating: {props.cleanliness_rating}</li>
+                </ul>
+            </div>
+            <img className='property-show-image' src={props.image_url} alt=''/>
+            <div className='property-show-review'>
+                <h2>This is the review part</h2>
+            </div>
+            
+            
         </div>
     )
 }
 
 export default PropertyShow
+
+// id={property.id}
+// review={property.review}
+// review_title={property.review_title}
+// township={property.township}
