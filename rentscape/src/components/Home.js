87055bc40363f8eb55e5111da68d43e08ca8a4c2
Cloudymolecule <baires82@gmail.com>
2021-06-02 20:44:17 -0400

    Apply css to home button

--- rentscape/src/components/Home.js
+++ rentscape/src/components/Home.js
@@ -1,12 +1,11 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
 
 const Home = () => {
     
     return (
         
         <div>
-            <><button onClick={() => window.history.back()}>Home</button></>
+            <button className='home-button' onClick={() => window.history.back()}>Home</button>
         </div>
     )
 }
